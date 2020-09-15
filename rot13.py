c8f80e145c648b97975c05423505409e0e5fc87a
nnnzo <44113165+nnnzo@users.noreply.github.com>
2020-09-15 14:21:39 +0200

    Update rot13.py

--- rot13.py
+++ rot13.py
@@ -7,3 +7,17 @@ def rot13(Clair):
             else:   crypt += chr(result)
         else :  crypt += " "
     print(crypt)
+        
+def rot13file():
+    crypt = ""
+    f = open("tocrypt.txt", "r")
+    e = open("crypted.txt", "w")
+    for d in f :
+        for r in d:
+            if r != " ":
+                result = ord(r)+13
+                if result > 122 :    crypt += chr(result - 26)
+                else:   crypt += chr(result)
+            else :  crypt += " "
+    e.write(crypt)
+    f.close
