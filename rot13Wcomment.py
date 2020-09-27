f700a3f9599fd21fbc9b0faaa5923918d974c869
nnnnnzo <44113165+nnnnnzo@users.noreply.github.com>
2020-09-27 16:48:00 +0200

    Update rot13Wcomment.py

--- rot13Wcomment.py
+++ rot13Wcomment.py
@@ -24,6 +24,21 @@ def rot13file():
     e.write(crypt) # on write la valeur de la variable crypt dans le fichier crypted.txt
     f.close # on ferme le fichier f
     e.close # on ferme le fichier e
+
+    def rot13fileuncrypt():
+    uncrypt = ""
+    f = open("todecrypt.txt", "r") # ouvre todecrypt.txt en lecture
+    e = open("uncrypted.txt", "w") # ouvre uncrypted.txt en écriture
+    for d in f : #pour le texte dans le fichier f
+        for r in d: # pour le caractère dans le texte du fichier f
+            if r != " ": # si le caractère est un espace
+                result = ord(r)-13 # on converti le caractères en ascii - 13
+                if result < 97 :    uncrypt += chr(result + 26) # si le ascii est < 97; on revient a la fin de l'alphabet
+                else:   uncrypt += chr(result) # on rajoute la lettre decrypté a la variable du texte decrypté final
+            else :  uncrypt += " " # si un espace on ajoute un espace
+    e.write(uncrypt) # on write la valeur de la variable crypt dans le fichier crypted.txt
+    f.close # on ferme le fichier f
+    e.close # on ferme le fichier e
     
 def v(txt):
     cle = "cledetest"
